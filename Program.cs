9573d93c272a3ecd4410064d428021abbb9f038e
AryuzinNS <121675745+AryuzinNS@users.noreply.github.com>
2025-04-12 12:00:38 +0300

    Hopefully fixed num2

--- Program.cs
+++ Program.cs
@@ -11,17 +11,14 @@ namespace Lab_8
     {
         static void Main(string[] args)
         {
-            string test = "William Shakespeare, widely regarded as one of the greatest writers in the English language, authored a total of 37 plays, along with numerous poems and sonnets. He was born in Stratford-upon-Avon, England, in 1564, and died in 1616. Shakespeare's most famous works, including \"Romeo and Juliet,\" \"Hamlet,\" \"Macbeth,\" and \"Othello,\" were written during the late 16th and early 17th centuries. \"Romeo and Juliet,\" a tragic tale of young love, was penned around 1595. \"Hamlet,\" one of his most celebrated tragedies, was written in the early 1600s, followed by \"Macbeth,\" a gripping drama exploring themes of ambition and power, around 1606. \"Othello,\" a tragedy revolving around jealousy and deceit, was also composed during this period, believed to be around 1603.";
-            var tp = new Purple_3(test);
+            string test = "1 июля 2015 года Греция объявила о дефолте по государственному долгу, став первой развитой страной в истории, которая не смогла выплатить свои долговые обязательства в полном объеме. Сумма дефолта составила порядка 1,6 миллиарда евро. Этому предшествовали долгие переговоры с международными кредиторами, такими как Международный валютный фонд (МВФ), Европейский центральный банк (ЕЦБ) и Европейская комиссия (ЕК), о программах финансовой помощи и реструктуризации долга. Основными причинами дефолта стали недостаточная эффективность реформ, направленных на улучшение финансовой стабильности страны, а также политическая нестабильность, что вызвало потерю доверия со стороны международных инвесторов и кредиторов. Последствия дефолта оказались глубокими и долгосрочными: сокращение кредитного рейтинга страны, увеличение затрат на заемный капитал, рост стоимости заимствований и утрата доверия со стороны международных инвесторов.";
+            var tp = new Purple_2(test);
             tp.Review();
-            Console.WriteLine(tp.Output);
-            foreach(var el in tp.Codes)
+            foreach(var str in tp.Output)
             {
-                Console.WriteLine(el.Item1 + " " + el.Item2.ToString());
+                Console.WriteLine(str);
             }
-            var tp2 = new Purple_4(tp.Output, tp.Codes);
-            tp2.Review();
-            Console.WriteLine(tp2.Output);
+            
         }
     }
 }
--- Purple_2.cs
+++ Purple_2.cs
@@ -9,111 +9,116 @@ using System.Xml.Serialization;
 
 namespace Lab_8
 {
-	public class Purple_2: Purple
-	{
-		private string[] _output;
-		public string[] Output
-		{
-			get
-			{
-				var newarr = new string[_output.Length];
-				Array.Copy(_output, newarr, _output.Length);
-				return newarr;
-			}
-		}
+    public class Purple_2: Purple
+    {
+    	private string[] _output;
+    	public string[] Output
+    	{
+    		get
+    		{
+    			var newarr = new string[_output.Length];
+    			Array.Copy(_output, newarr, _output.Length);
+    			return newarr;
+    		}
+    	}
 
-		public Purple_2(string input): base(input){}
+    	public Purple_2(string input): base(input){}
 
-		public override void Review()
-		{
-			if(Input  == null)
-			{
-				return;
-			}
+    	public override void Review()
+    	{
+    		if(Input  == null)
+    		{
+    			return;
+    		}
 
-			string[] splitted = Input.Trim().Replace('\n', ' ').Split();
-			var result = new StringBuilder();
-			var current = new StringBuilder();
+    		string[] splitted = Input.Trim().Replace('\n', ' ').Split();
+    		var result = new StringBuilder();
+    		var current = new StringBuilder();
 
-			for(int i = 0; i < splitted.Length; ++i)
-			{
-				var word = splitted[i];
-				if(word.Length == 0)
-				{
-					continue;
-				}
+    		for(int i = 0; i < splitted.Length; i++)
+    		{
 
+                if (splitted[i].Length == 0)
+    			{
+    				continue;
+    			}
+                var word = splitted[i];
 
-				int n = current.Length;
-				if ((n + word.Length + 1 <= 50) || (n == 0 && word.Length <= 50))
-				{
-					if (n > 0)
-					{
-						current.Append(' ');
-					}
-					current.Append(word);
-				}
+    			int n = current.Length;
+    			if ((n + word.Length + 1 <= 50) || (n == 0 && word.Length <= 50))
+    			{
+    				if (n > 0)
+    				{
+    					current.Append(' ');
+    				}
+    				current.Append(word);
+    			}
 
-				if( n+ word.Length + 1 > 50 || i == splitted.Length - 1)
-				{
-					if(result.Length > 0)
-					{
-						result.Append('\n');
-					}
-					var cursplit = current.ToString().Split();
-					int spaces = cursplit.Length - 1;
-					int remain = 50 - current.Length;
-					if(remain < 0)
-					{
-						remain = 0;
-					}
-					int minsp, additionalspaces;
-					if(spaces > 0)
-					{
-						minsp = 1 + remain / spaces;
-						additionalspaces = remain % spaces;
-					}
-					else
-					{
-						minsp = remain;
-						additionalspaces = 0;
-					}
-					
-					for(int j = 0; j < spaces; ++j)
-					{
-						result.Append(cursplit[j]);
-						int curspaces = minsp;
-						if(j < additionalspaces)
-						{
-							curspaces++;
-						}
-						result.Append(new string(' ', curspaces));
-					}
-					result.Append(cursplit[cursplit.Length - 1]);
-					if(spaces == 0)
-					{
-                        result.Append(new string(' ', remain));
+    			if( (n+ word.Length + 1 > 50) || (i == splitted.Length - 1))
+    			{
+    				if(result.Length > 0)
+    				{
+    					result.Append('\n');
+    				}
+    				var cursplit = current.ToString().Split();
+    				int spaces = cursplit.Length - 1;
+    				int remain = 50 - current.Length;
+    				if(remain < 0)
+    				{
+    					remain = 0;
+    				}
+    				int minsp, additionalspaces;
+    				if(spaces <= 0)
+    				{
+    					
+                        minsp = remain;
+    					additionalspaces = 0;
+    				}
+    				else
+    				{
+    					
+                        minsp = 1 + remain / spaces;
+                        additionalspaces = remain % spaces;
                     }
 
-					current.Clear();
-					current.Append(word);
+    				for(int j = 0; j < spaces; j++)
+    				{
+    					result.Append(cursplit[j]);
+    					int curspaces = minsp;
+    					if(j < additionalspaces)
+    					{
+    						curspaces++;
+    					}
+    					result.Append(new string(' ', curspaces));
+    				}
+    				result.Append(cursplit[^1]);
+    				if(spaces == 0)
+    				{
+                           result.Append(new string(' ', remain));
+                    }
 
-					if (n + word.Length + 1 > 50 && i == cursplit.Length - 1)
-					{
-						result.Append("\n" + current);
-					}
+    				current.Clear();
+    				current.Append(word);
 
-				}
-			}
-			_output = result.ToString().Split('\n');
-		}
+    				if (n + 1+ word.Length > 50 && i == splitted.Length - 1)
+    				{
+    					result.Append("\n" + current);
+    				}
 
-        public override string ToString()
-        {
-            if(_output == null) { return null; }
-			return String.Join('\n', _output);
-        }
+    			}
+    		}
+    		_output = result.ToString().Split('\n');
+    	}
 
+           public override string ToString()
+           {
+               if(_output == null) { 
+                return null; 
+               }
+    		   return String.Join('\n', _output);
+           }
 
-    }
+       }
+
+    
 }
